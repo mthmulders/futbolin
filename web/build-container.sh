a51ad50a0439b894d974335a3c51ef144513c279
Maarten Mulders <m.th.mulders@xs4all.nl>
2019-01-04 20:55:34 +0100

    Tag each version of the demo page separately

--- web/build-container.sh
+++ web/build-container.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+if [[ "$#" -ne 1 ]]; then
+    echo "$0 <num>"
+    echo ""
+    echo "Parameters:"
+    echo "  <num> the version of the container being built"
+    exit
+fi
+
 docker build -f Dockerfile -t futbolinregistry.azurecr.io/demo-page .
 docker push futbolinregistry.azurecr.io/demo-page
