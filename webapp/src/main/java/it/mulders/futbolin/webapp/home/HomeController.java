d8996c19f850fe51e93d55aee035c435884dfa7e
Maarten Mulders <m.th.mulders@xs4all.nl>
2020-01-03 13:33:57 +0100

    Add a few unit tests

--- webapp/src/main/java/it/mulders/futbolin/webapp/home/HomeController.java
+++ webapp/src/main/java/it/mulders/futbolin/webapp/home/HomeController.java
@@ -1,5 +1,7 @@
 package it.mulders.futbolin.webapp.home;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.enterprise.context.RequestScoped;
@@ -11,20 +13,24 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
+import static lombok.AccessLevel.PACKAGE;
+
+@AllArgsConstructor(access = PACKAGE)
 @Controller
+@NoArgsConstructor
 @Path("home")
 @RequestScoped
 @Slf4j
 public class HomeController {
-    @Inject Models models;
+    @Inject Models model;
     @Inject OverviewService overviewService;
 
     @GET
     @Produces("text/html; charset=UTF-8")
     @View("home.jsp")
     public void show() {
-        models.put("teams", overviewService.getMyTeams());
-        models.put("tournaments", overviewService.getMyTournaments());
-        models.put("matches", overviewService.getMyMatches());
+        model.put("teams", overviewService.getMyTeams());
+        model.put("tournaments", overviewService.getMyTournaments());
+        model.put("matches", overviewService.getMyMatches());
     }
 }
---
+++ webapp/src/test/java/it/mulders/futbolin/webapp/home/HomeControllerTest.java
@@ -0,0 +1,65 @@
+package it.mulders.futbolin.webapp.home;
+
+import org.assertj.core.api.WithAssertions;
+import org.eclipse.krazo.core.ModelsImpl;
+import org.junit.jupiter.api.Test;
+
+import javax.mvc.Models;
+
+import java.util.Collection;
+import java.util.List;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+class HomeControllerTest implements WithAssertions {
+    private final OverviewService service = mock(OverviewService.class);
+    private final Models model = new ModelsImpl();
+
+    private final HomeController controller = new HomeController(model, service);
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void show_modelShouldHaveTeams() {
+        // Arrange
+        final Collection<String> teams = List.of("My Team");
+        when(service.getMyTeams()).thenReturn(teams);
+
+        // Act
+        controller.show();
+
+        // Assert
+        assertThat(controller.model.get("teams")).isInstanceOf(Collection.class);
+        assertThat((Collection<String>) controller.model.get("teams")).containsAll(teams);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void show_modelShouldHaveTournaments() {
+        // Arrange
+        final Collection<String> tournaments = List.of("My Tournament");
+        when(service.getMyTournaments()).thenReturn(tournaments);
+
+        // Act
+        controller.show();
+
+        // Assert
+        assertThat(controller.model.get("teams")).isInstanceOf(Collection.class);
+        assertThat((Collection<String>) controller.model.get("tournaments")).containsAll(tournaments);
+    }
+
+    @Test
+    @SuppressWarnings("unchecked")
+    void show_modelShouldHaveMatches() {
+        // Arrange
+        final Collection<String> matches = List.of("My Match");
+        when(service.getMyMatches()).thenReturn(matches);
+
+        // Act
+        controller.show();
+
+        // Assert
+        assertThat(controller.model.get("matches")).isInstanceOf(Collection.class);
+        assertThat((Collection<String>) controller.model.get("matches")).containsAll(matches);
+    }
+}
