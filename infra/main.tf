a3d9ff8326dda4420b8287b01b326356e28680d9
Maarten Mulders <m.th.mulders@xs4all.nl>
2021-02-22 15:29:45 +0100

    Remove Kubernetes provider

--- infra/main.tf
+++ infra/main.tf
@@ -1,9 +1,5 @@
 terraform {
   required_providers {
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = "2.0.2"
-    }
     local = {
       source  = "hashicorp/local"
       version = "2.1.0"
